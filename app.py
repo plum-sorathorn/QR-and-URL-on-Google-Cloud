5da9c4845f929d704792b332e24f9d66470c39b1
Sorathorn Thongpitukthavorn <sorathorn.asb@gmail.com>
2025-04-13 23:45:48 -0400

    Update app.py, change ports to support cloudflare DNS

--- app.py
+++ app.py
@@ -116,4 +116,4 @@ def get_qr_code(short_code):
     return redirect(qr_url)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 443)))
